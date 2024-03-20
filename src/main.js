08e38c7834aa13f89df24fb864a50c92c2021fa9
BrianMarchese <brianmarchese@hotmail.com>
2024-03-20 08:36:43 -0300

    commit final, footer agregado

--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ function Menu(e){
     let list = document.querySelector('ul');
     e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
 }
+document.getElementById("copyright").textContent = "Copyright © " + new Date().getFullYear() + " Brian Marchese. " + " Todos los derechos reservados ";
